b9946ed8cbcc5a1cec79acec5e68b2d34798c253
Leon Timmermans <fawaka@gmail.com>
2011-09-10 10:19:17 +0200

    Converted to Dist::Zilla

--- lib/SysV/SharedMem.pm
+++ lib/SysV/SharedMem.pm
@@ -11,8 +11,7 @@ use Sub::Exporter -setup => { exports => [qw/shared_open shared_remove shared_st
 
 use XSLoader;
 
-our $VERSION = '0.005';
-XSLoader::load(__PACKAGE__, $VERSION);
+XSLoader::load(__PACKAGE__, __PACKAGE__->VERSION);
 
 const my %flags_for => (
 	'<'  => SHM_RDONLY,
@@ -49,13 +48,7 @@ sub shared_open {
 
 __END__
 
-=head1 NAME
-
-SysV::SharedMem - SysV Shared memory made easy
-
-=head1 VERSION
-
-Version 0.005
+# ABSTRACT: SysV Shared memory made easy
 
 =head1 SYNOPSIS
 
@@ -67,9 +60,7 @@ Version 0.005
 
 This module maps shared memory into a variable that can be read just like any other variable, and it can be written to using standard Perl techniques such as regexps and C<substr>, B<as long as they don't change the length of the variable>.
 
-=head1 METHODS
-
-=head2 shared_open($var, $filename, $mode, %options)
+=func shared_open($var, $filename, $mode, %options)
 
 Open a shared memory object named C<$filename> and attach it to C<$var>. $filename must be the path to an existing file or undef, in which case the C<key> option is used. C<$mode> determines the read/write mode. It works the same as in open.
 
@@ -99,11 +90,11 @@ The project id, used to ensure the key generated from the filename is unique. On
 
 =back
 
-=head2 shared_remove($var)
+=func shared_remove($var)
 
 Marks a memory object to be removed. Shared memory has kernel persisence so it has to be explicitly disposed of. One can still use the object after marking it for removal.
 
-=head2 shared_stat($var)
+=func shared_stat($var)
 
 Retrieve the properties of the shared memory object. It returns a hashref with these members:
 
@@ -159,63 +150,12 @@ Time of last of control structure
 
 =back
 
-=head2 shared_chmod($var, $modebits)
+=func shared_chmod($var, $modebits)
 
 Change the (lower 9) modebits of the shared memory object.
 
-=head2 shared_chown($var, $uid, $gid = undef)
+=func shared_chown($var, $uid, $gid = undef)
 
 Change the owning uid and optionally gid of the shared memory object.
-
-=head1 AUTHOR
-
-Leon Timmermans, C<< <leont at cpan.org> >>
-
-=head1 BUGS
-
-Please report any bugs or feature requests to C<bug-sysv-sharedmem at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=SysV-SharedMem>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
-
-=head1 SUPPORT
-
-You can find documentation for this module with the perldoc command.
-
-    perldoc SysV::SharedMem
-
-
-You can also look for information at:
-
-=over 4
-
-=item * RT: CPAN's request tracker
-
-L<http://rt.cpan.org/NoAuth/Bugs.html?Dist=SysV-SharedMem>
-
-=item * AnnoCPAN: Annotated CPAN documentation
-
-L<http://annocpan.org/dist/SysV-SharedMem>
-
-=item * CPAN Ratings
-
-L<http://cpanratings.perl.org/d/SysV-SharedMem>
-
-=item * Search CPAN
-
-L<http://search.cpan.org/dist/SysV-SharedMem/>
-
-=back
-
-=head1 ACKNOWLEDGEMENTS
-
-=head1 LICENSE AND COPYRIGHT
-
-Copyright 2010 Leon Timmermans.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
 
 =cut
